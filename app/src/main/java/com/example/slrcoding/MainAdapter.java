cca4c88e06be518375d65f725e8689fbfc726181
leejeongchan <dlwjdcks5343@naver.com>
2019-05-13 20:39:51 +0900

    Merge branch 'Mox' of https://github.com/dudcheol/SLRcoding into JeongChan
    # Conflicts:
    #	app/src/main/java/com/example/slrcoding/fragment/Feed_Child_FragmentTwo.java
    #	app/src/main/res/layout/fragment_board.xml
    #	app/src/main/res/layout/fragment_mypage.xml
    피드 상세보기 디자인 추가+ 피드 디자인 변경+ 피드 클릭 이벤트 테스트

--- app/src/main/java/com/example/slrcoding/MainAdapter.java
+++ app/src/main/java/com/example/slrcoding/MainAdapter.java
@@ -1,11 +1,13 @@
 package com.example.slrcoding;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -27,11 +29,24 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder
     }
     //Board로 데이터 바인딩 시킴 이로인해 item_main에 내용이 뜨게 됨
     @Override
-    public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
-        Board data = mBoardList.get(position);
+    public void onBindViewHolder(@NonNull MainViewHolder holder, final int position) {
+
+
+
+        final Board data = mBoardList.get(position);
+
         holder.mCategoryTextView.setText(data.getCategory());
         holder.mTitleTextView.setText(data.getTitle());
         holder.mNameTextView.setText(data.getName());
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                //여기서 각 피드들 클릭시 상세보기로 이동
+                Toast.makeText(context, position +""+data.getTitle(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -42,6 +57,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder
     //하나당 세부 내용을 위한 뷰 홀더
     //item_main 아이디를 통해 각 바인딩할 뷰들을 얻어옴
     class MainViewHolder extends RecyclerView.ViewHolder{
+        public final View mView;
         private TextView mCategoryTextView;
         private TextView mTitleTextView;
         private TextView mNameTextView;
@@ -50,7 +66,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder
             mCategoryTextView = itemView.findViewById(R.id.category_name);
             mTitleTextView = itemView.findViewById(R.id.item_title_text);
             mNameTextView = itemView.findViewById(R.id.item_name_text);
-
+            mView = itemView;
 
         }
     }
--- app/src/main/java/com/example/slrcoding/fragment/Feed_Child_FragmentTwo.java
+++ app/src/main/java/com/example/slrcoding/fragment/Feed_Child_FragmentTwo.java
@@ -1,5 +1,6 @@
 package com.example.slrcoding.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.slrcoding.Board;
+import com.example.slrcoding.FeedWriteActivity;
 import com.example.slrcoding.MainAdapter;
 import com.example.slrcoding.R;
 
@@ -21,7 +23,7 @@ public class Feed_Child_FragmentTwo extends Fragment {
     private RecyclerView mMainRecyclerView;
     private MainAdapter mAdapter;
     private List<Board> mBoardList;
-
+    public static final int REQUEST_CODE = 1000;
     public Feed_Child_FragmentTwo() {
         // Required empty public constructor
     }
@@ -30,8 +32,15 @@ public class Feed_Child_FragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_feed__child__fragment_two, container, false);
-        mMainRecyclerView = rootView.findViewById(R.id.main_recycler_view);
+        mMainRecyclerView = rootView.findViewById(R.id.main_recycler_view2);
 
+        rootView.findViewById(R.id.main_write_button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FeedWriteActivity.class);
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
         //container.findViewById(R.id.main_write_button).setOnClickListener(this);
         //피드 글 적용시키기
         mBoardList = new ArrayList<>();
