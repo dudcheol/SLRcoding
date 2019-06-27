3126c8b64bfccdf30572d44977d38a7e30b08d9e
dudcheol <parkyc0301@gmail.com>
2019-06-27 20:35:54 +0900

    파이어베이스 연동 # 파이어베이스와 연동함 # migrate to androidX 했음

--- app/src/main/java/com/example/slrcoding/SignUpActivity.java
+++ app/src/main/java/com/example/slrcoding/SignUpActivity.java
@@ -1,12 +1,9 @@
 package com.example.slrcoding;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
--- app/src/main/java/com/example/slrcoding/fragment/Feed_Child_FragmentTwo.java
+++ app/src/main/java/com/example/slrcoding/fragment/Feed_Child_FragmentTwo.java
@@ -1,15 +1,13 @@
 package com.example.slrcoding.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.slrcoding.Board;
-import com.example.slrcoding.FeedWriteActivity;
 import com.example.slrcoding.MainAdapter;
 import com.example.slrcoding.R;
 
