0f0e1e18fa4077d0432ec917ba7dea4c4984a5fa
Min choel <tmxk4197@naver.com>
2019-07-10 15:15:24 +0900

    mincheol 190710

---
+++ app/src/main/java/com/example/slrcoding/Mypage_sub0_Comment.java
@@ -0,0 +1,13 @@
+package com.example.slrcoding;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Mypage_sub0_Comment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage_sub0_comment);
+    }
+}
