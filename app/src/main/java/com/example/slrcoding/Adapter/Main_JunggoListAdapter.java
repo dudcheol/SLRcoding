63e7914e1087dbe2c64557240b99f65543ef6c0d
leejeongchan <dlwjdcks5343@naver.com>
2019-08-01 23:52:02 +0900

    마스터 최신 풀가져온것2

---
+++ app/src/main/java/com/example/slrcoding/Adapter/Main_JunggoListAdapter.java
@@ -0,0 +1,62 @@
+package com.example.slrcoding.Adapter;
+
+import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.slrcoding.R;
+import com.example.slrcoding.VO.Main_JunggoVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_JunggoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    List<Main_JunggoVO> testList;
+    Activity activity;
+    View v;
+
+    public Main_JunggoListAdapter(Activity context, List<Main_JunggoVO> junggos) {
+        this.testList = junggos;
+        this.activity = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        v = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_junggo_item,parent,false);
+        return new ItemViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((ItemViewHolder)holder).title.setText(testList.get(position).getNum()+"");
+
+        /*GradientDrawable drawable=(GradientDrawable)v.getContext().getDrawable(R.drawable.background_rounding_top);
+        ((ItemViewHolder) holder).image.setBackground(drawable);
+        ((ItemViewHolder) holder).image.setClipToOutline(true);*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return testList.size();
+    }
+
+    // 커스텀 뷰홀더
+    class ItemViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView image;
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.junggo_image);
+            title = itemView.findViewById(R.id.junggo_title);
+        }
+    }
+}
