db10341719e88fa67a67d3c1bdb3ffbb5a725f07
Min choel <tmxk4197@naver.com>
2019-08-22 17:40:08 +0900

    1. 알림 서비스 일부 구현 -> 아직 테스트용으로 백그라운드일 때만 수신되게 만듦

--- app/src/main/java/com/example/slrcoding/MainActivity.java
+++ app/src/main/java/com/example/slrcoding/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.slrcoding.fragment.BoardFragment;
 import com.example.slrcoding.fragment.FeedFragment;
@@ -26,6 +29,12 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     int flag=0;
@@ -168,4 +177,5 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
     }
+
 }
