b7003fab382c8f0185918516399475b4af49e48e
leejeongchan <dlwjdcks5343@naver.com>
2019-08-26 00:08:28 +0900

    미팅어댑터에서 채팅룸 인텐트 시키기

---
+++ app/src/main/java/com/example/slrcoding/Activity/ChatRoomActivity.java
@@ -0,0 +1,23 @@
+package com.example.slrcoding.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.slrcoding.R;
+
+/**
+ * 채팅방
+ * 여기서 우선 상대방 아이디를 받아오다.
+ * @author 이정찬
+ */
+public class ChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+
+
+    }
+}
